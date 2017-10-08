fe65a2fdbc6e8099b8c308cafdaa299469efd081
kwanulee <kwlee@hansung.ac.kr>
2017-10-08 16:15:04 +0900

    Adapter 예제 추가

---
+++ adapter/ObjectAdapterExampleProblem/src/hansung/designpatterns/adapter/ExistingSystem.java
@@ -0,0 +1,10 @@
+package hansung.designpatterns.adapter;
+
+public class ExistingSystem {
+
+	public static void main(String[] args) {
+		VenderClass vender = new VenderClassAdapter(new Vender2Class());
+		vender.request();
+	}
+
+}
